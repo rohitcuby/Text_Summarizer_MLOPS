d28ab5ef14820e8241d92c05a2c460c3efc476a7
rohitcuby <rohit.mehar.cse21@itbhu.ac.in>
2024-07-31 12:55:05 +0530

    website created

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, render_template
+from TextSummarizer.pipeline.prediction import PredictionPipeline
+
+app = Flask(__name__)
+pipeline = PredictionPipeline()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    text = data.get('text', '')
+    if not text:
+        return jsonify({'error': 'Text is required'}), 400
+    summary = pipeline.predict(text)
+    return jsonify({'summary': summary})
+
+if __name__ == '__main__':
+    app.run(debug=True)
